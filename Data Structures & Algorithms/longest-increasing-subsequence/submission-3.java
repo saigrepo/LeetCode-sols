ec6bea4248a79dfede5f6d8276d1f434ddaf4ba9
Saiganesh_H <129721253+saigrepo@users.noreply.github.com>
2026-08-06 21:55:15 +0530

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[] dp;
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        dp = new int[len];
+        Arrays.fill(dp, 1);
+        for(int i=0;i<len;i++) {
+            for(int j=0;j<i;j++) {
+                if(nums[j]<nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+       //System.out.println( Arrays.toString(dp));
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    //plain recursion
+    // int rec(int[] arr, int idx, int prev) {
+    //     if(idx>arr.length) return -1;
+    //     if(idx==arr.length) return 0;
+
+    //     if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
+
+    //     int skip = rec(arr, idx+1, prev);
+
+    //     if(prev==-1 || arr[prev]<arr[idx]) {
+    //         skip = Math.max(skip, 1 + rec(arr, idx+1, idx));
+    //     }
+    //     return dp[idx][prev+1]=skip;
+    // }
+}
